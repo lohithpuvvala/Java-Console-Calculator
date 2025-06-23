0b72faa59fe1f7ad9feb578939c1fa52abb1c2b8
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-23 13:07:54 +0530

    Create Basic Arthematic Java Console Calculator

---
+++ com/lohithpuvvala/Main.java
@@ -0,0 +1,59 @@
+package com.lohithpuvvala;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("--------------------------------------------------");
+            System.out.println("|       Welcome to Java Console Calculator       |");
+            System.out.println("--------------------------------------------------");
+
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Enter an integer: ");
+            double a = in.nextInt();
+            System.out.print("Enter an integer: ");
+            double b = in.nextInt(); in.nextLine();
+            System.out.print("Enter the Operation to Perform:\n" +
+                    " + : Addition Operation\n" +
+                    " - : Subtraction Operation\n" +
+                    " * : Multiplication Operation\n" +
+                    " / : Division Operation\n" +
+                    " % : Modulus Operation\n" +
+                    "Choose any one option: ");
+            String op = in.nextLine();
+            double result = 0;
+
+            boolean valid = true;
+            switch (op) {
+                case "+": result = a + b; break;
+                case "-": result = a - b; break;
+                case "*": result = a * b; break;
+                case "/":
+                    if(b != 0) {
+                        result = a / b;
+                    }else{
+                        System.out.println("Error: Division by zero is not allowed.");
+                        valid = false;
+                    }
+                    break;
+                case "%": if(b != 0){
+                    result = a % b;
+                }else{
+                    System.out.println("Error: Modulus by zero is not allowed.");
+                    valid = false;
+                } break;
+                default:
+                    System.out.println("Error: Invalid Operation entered.");
+                    valid = false;
+            }
+
+            if(valid) System.out.printf("Result: %.3f \n",result);
+            System.out.println("To quit the program, enter 'q'. Press any other key to reload the calculator: ");
+            char quit = in.next().charAt(0);
+            if(quit == 'q' || quit == 'Q') break;
+        }
+    }
+}
